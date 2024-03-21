6ad9e1edaeb0c05c8e735c8cbcc854725345743c
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-03-21 14:01:22 +0800

    ci: remove single file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-	plugins: [viteSingleFile()],
+	// plugins: [viteSingleFile()],
   base: "/concept-annotator/", // https://vitejs.dev/guide/static-deploy.html#github-pages
 });
